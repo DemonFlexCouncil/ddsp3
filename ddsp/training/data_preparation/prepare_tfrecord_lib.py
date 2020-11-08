63f4709eb9c2ef51714efdab5c5a95775c73ba90
Demon Flex Council <65683126+DemonFlexCouncil@users.noreply.github.com>
2020-11-07 16:18:42 -0800

    Update prepare_tfrecord_lib.py

--- ddsp/training/data_preparation/prepare_tfrecord_lib.py
+++ ddsp/training/data_preparation/prepare_tfrecord_lib.py
@@ -62,7 +62,7 @@ def _load_audio(audio_path, sample_rate):
   return {'audio': audio}
 
 
-def add_loudness(ex, sample_rate, frame_rate, n_fft=2048):
+def add_loudness(ex, sample_rate, frame_rate, n_fft=6144):
   """Add loudness in dB."""
   beam.metrics.Metrics.counter('prepare-tfrecord', 'compute-loudness').inc()
   audio = ex['audio']
